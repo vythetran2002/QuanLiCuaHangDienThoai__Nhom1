e66c28cb59950a2bda7ea08c4bb863d14f2830f2
duynhut908 <dnhut908@gmail.com>
2022-10-30 13:21:33 +0700

    update

--- QuanLiCuaHangDienThoai/Forms/fStaffMain.Designer.cs
+++ QuanLiCuaHangDienThoai/Forms/fStaffMain.Designer.cs
@@ -35,7 +35,7 @@
             // 
             this.button1.Location = new System.Drawing.Point(128, 89);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(431, 187);
+            this.button1.Size = new System.Drawing.Size(236, 187);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
