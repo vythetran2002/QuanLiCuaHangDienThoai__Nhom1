ba9ff5703ad9429aef0df9905d45de9a031c9cfe
halo51102 <20110667@student.hcmute.edu.vn>
2022-10-31 17:41:18 +0700

    sửa: form Quản Lý

--- QuanLiCuaHangDienThoai/Forms/QuanLy.cs
+++ QuanLiCuaHangDienThoai/Forms/QuanLy.cs
@@ -17,11 +17,11 @@ namespace QuanLiCuaHangDienThoai.Forms
         public QuanLy()
         {
             InitializeComponent();
-            
+            Load_DateTimePicker_DoanhThu();
         }
         private void QuanLy_Load(object sender, EventArgs e)
         {
-            LoadData();
+            //LoadData();
         }
         void LoadData()
         {
@@ -34,10 +34,9 @@ namespace QuanLiCuaHangDienThoai.Forms
                                                  MaSP = u.maSP,
                                                  TenSP = u.tenSP,
                                                  Gia = u.gia,
-                                                 //GiaKM = u.GiaKM,
                                                  SoLuong = u.soLuong,
-                                                 MaDM = u.maDM,
-                                                 MaNCC = u.maNCC
+                                                 //MaDM = u.maDM,
+                                                 //MaNCC = u.maNCC
                                              };
                 dataGridView_DM.DataSource = from u in db.DANHMUCs
                                              select new
@@ -49,7 +48,9 @@ namespace QuanLiCuaHangDienThoai.Forms
                                              select new
                                              {
                                                  Username = u.username,
+                                                 Ten = u.ten,
                                                  Password = u.mk,
+                                                 Loai = u.loai,
                                                  NgayTao = u.ngayTao
                                              };
             }
@@ -62,6 +63,40 @@ namespace QuanLiCuaHangDienThoai.Forms
             dataGridView_SP.AutoResizeColumns();
             dataGridView_DM.AutoResizeColumns();
             dataGridView_TK.AutoResizeColumns();
+
+            SanPhamBinding();
+            DanhMucBinding();
+            TaiKhoanBinding();
+            
+        }
+        void SanPhamBinding()
+        {
+            textBox_MaSP.DataBindings.Add("Text", dataGridView_SP.DataSource, "MaSP", true, DataSourceUpdateMode.Never);
+            textBox_TenSP.DataBindings.Add("Text", dataGridView_SP.DataSource, "TenSP", true, DataSourceUpdateMode.Never);
+            textBox_Gia.DataBindings.Add("Value", dataGridView_SP.DataSource, "Gia", true, DataSourceUpdateMode.Never);
+            textBox_SoLuong.DataBindings.Add("Value", dataGridView_SP.DataSource, "SoLuong", true, DataSourceUpdateMode.Never);
+        }
+
+        void DanhMucBinding()
+        {
+            textBox_MaDM.DataBindings.Add("Text", dataGridView_DM.DataSource, "MaDM", true, DataSourceUpdateMode.Never);
+            textBox_TenDM.DataBindings.Add("Text", dataGridView_DM.DataSource, "TenDM", true, DataSourceUpdateMode.Never);
+        }
+
+        void TaiKhoanBinding()
+        {
+            textBox_Username.DataBindings.Add("Text", dataGridView_SP.DataSource, "Username", true, DataSourceUpdateMode.Never);
+            textBox_Ten.DataBindings.Add("Text", dataGridView_SP.DataSource, "Ten", true, DataSourceUpdateMode.Never);
+            textBox_Password.DataBindings.Add("Text", dataGridView_SP.DataSource, "Password", true, DataSourceUpdateMode.Never);
+            textBox_Loai.DataBindings.Add("Text", dataGridView_SP.DataSource, "Loai", true, DataSourceUpdateMode.Never);
+            //textBox_NgayTao.DataBindings.Add("Text", dataGridView_SP.DataSource, "Ten", true, DataSourceUpdateMode.Never);
+        }
+
+        void Load_DateTimePicker_DoanhThu()
+        {
+            DateTime today = DateTime.Now;
+            dateTimePicker_FromDate.Value = new DateTime(today.Year, today.Month, 1);
+            dateTimePicker_ToDate.Value = dateTimePicker_FromDate.Value.AddMonths(1).AddDays(-1);
         }
         private void button_Xoa_Click(object sender, EventArgs e)
         {
