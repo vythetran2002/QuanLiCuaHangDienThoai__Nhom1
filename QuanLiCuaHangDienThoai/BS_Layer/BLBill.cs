aaa628d977540b28df48e7f80315341fda5007af
duynhut908 <dnhut908@gmail.com>
2022-10-31 21:55:22 +0700

    Staff + UC phone + BLBill

---
+++ QuanLiCuaHangDienThoai/BS_Layer/BLBill.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+namespace QuanLiCuaHangDienThoai.BS_Layer
+{
+    internal class BLBill
+    {
+        public System.Data.Linq.Table<HOADON> LayHoaDon()
+        {
+            QLDTDataContext q = new QLDTDataContext();
+            return q.HOADONs;
+        }
+
+
+    }
+}
--- QuanLiCuaHangDienThoai/Forms/fStaffMain.Designer.cs
+++ QuanLiCuaHangDienThoai/Forms/fStaffMain.Designer.cs
@@ -28,32 +28,189 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lb_Gia = new System.Windows.Forms.Label();
+            this.lb_MaSP = new System.Windows.Forms.Label();
+            this.btn_AddHDCT = new System.Windows.Forms.Button();
+            this.lb_DM = new System.Windows.Forms.Label();
+            this.lb_TenSP = new System.Windows.Forms.Label();
+            this.lb_NCC = new System.Windows.Forms.Label();
+            this.flp_Phone = new System.Windows.Forms.FlowLayoutPanel();
+            this.btn_ThanhToan = new System.Windows.Forms.Button();
+            this.btn_NewHD = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.flp_HDCT = new System.Windows.Forms.FlowLayoutPanel();
+            this.btn_LoadHDCT = new System.Windows.Forms.Button();
+            this.lb_MaHD = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // panel1
             // 
-            this.button1.Location = new System.Drawing.Point(128, 89);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(431, 187);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.panel1.Controls.Add(this.lb_Gia);
+            this.panel1.Controls.Add(this.lb_MaSP);
+            this.panel1.Controls.Add(this.btn_AddHDCT);
+            this.panel1.Controls.Add(this.lb_DM);
+            this.panel1.Controls.Add(this.lb_TenSP);
+            this.panel1.Controls.Add(this.lb_NCC);
+            this.panel1.Location = new System.Drawing.Point(1091, 69);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(513, 199);
+            this.panel1.TabIndex = 13;
+            // 
+            // lb_Gia
+            // 
+            this.lb_Gia.AutoSize = true;
+            this.lb_Gia.Location = new System.Drawing.Point(3, 116);
+            this.lb_Gia.Name = "lb_Gia";
+            this.lb_Gia.Size = new System.Drawing.Size(51, 20);
+            this.lb_Gia.TabIndex = 9;
+            this.lb_Gia.Text = "label5";
+            // 
+            // lb_MaSP
+            // 
+            this.lb_MaSP.AutoSize = true;
+            this.lb_MaSP.Location = new System.Drawing.Point(3, 17);
+            this.lb_MaSP.Name = "lb_MaSP";
+            this.lb_MaSP.Size = new System.Drawing.Size(51, 20);
+            this.lb_MaSP.TabIndex = 8;
+            this.lb_MaSP.Text = "label4";
+            // 
+            // btn_AddHDCT
+            // 
+            this.btn_AddHDCT.Location = new System.Drawing.Point(321, 158);
+            this.btn_AddHDCT.Name = "btn_AddHDCT";
+            this.btn_AddHDCT.Size = new System.Drawing.Size(189, 38);
+            this.btn_AddHDCT.TabIndex = 7;
+            this.btn_AddHDCT.Text = "Thêm SP vào HĐ";
+            this.btn_AddHDCT.UseVisualStyleBackColor = true;
+            // 
+            // lb_DM
+            // 
+            this.lb_DM.AutoSize = true;
+            this.lb_DM.Location = new System.Drawing.Point(3, 83);
+            this.lb_DM.Name = "lb_DM";
+            this.lb_DM.Size = new System.Drawing.Size(51, 20);
+            this.lb_DM.TabIndex = 2;
+            this.lb_DM.Text = "label3";
+            // 
+            // lb_TenSP
+            // 
+            this.lb_TenSP.AutoSize = true;
+            this.lb_TenSP.Location = new System.Drawing.Point(3, 49);
+            this.lb_TenSP.Name = "lb_TenSP";
+            this.lb_TenSP.Size = new System.Drawing.Size(51, 20);
+            this.lb_TenSP.TabIndex = 1;
+            this.lb_TenSP.Text = "label2";
+            // 
+            // lb_NCC
+            // 
+            this.lb_NCC.AutoSize = true;
+            this.lb_NCC.Location = new System.Drawing.Point(231, 83);
+            this.lb_NCC.Name = "lb_NCC";
+            this.lb_NCC.Size = new System.Drawing.Size(51, 20);
+            this.lb_NCC.TabIndex = 0;
+            this.lb_NCC.Text = "label1";
+            // 
+            // flp_Phone
+            // 
+            this.flp_Phone.AutoScroll = true;
+            this.flp_Phone.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.flp_Phone.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.flp_Phone.Location = new System.Drawing.Point(12, 70);
+            this.flp_Phone.Name = "flp_Phone";
+            this.flp_Phone.Size = new System.Drawing.Size(1073, 537);
+            this.flp_Phone.TabIndex = 10;
+            // 
+            // btn_ThanhToan
+            // 
+            this.btn_ThanhToan.Location = new System.Drawing.Point(1471, 274);
+            this.btn_ThanhToan.Name = "btn_ThanhToan";
+            this.btn_ThanhToan.Size = new System.Drawing.Size(133, 48);
+            this.btn_ThanhToan.TabIndex = 18;
+            this.btn_ThanhToan.Text = "Thanh Toán";
+            this.btn_ThanhToan.UseVisualStyleBackColor = true;
+            // 
+            // btn_NewHD
+            // 
+            this.btn_NewHD.Location = new System.Drawing.Point(1091, 274);
+            this.btn_NewHD.Name = "btn_NewHD";
+            this.btn_NewHD.Size = new System.Drawing.Size(127, 48);
+            this.btn_NewHD.TabIndex = 17;
+            this.btn_NewHD.Text = "HĐ mới";
+            this.btn_NewHD.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(1127, 28);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(270, 26);
+            this.dateTimePicker1.TabIndex = 16;
+            // 
+            // flp_HDCT
+            // 
+            this.flp_HDCT.AutoScroll = true;
+            this.flp_HDCT.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.flp_HDCT.Location = new System.Drawing.Point(1091, 371);
+            this.flp_HDCT.Name = "flp_HDCT";
+            this.flp_HDCT.Size = new System.Drawing.Size(513, 236);
+            this.flp_HDCT.TabIndex = 15;
+            // 
+            // btn_LoadHDCT
+            // 
+            this.btn_LoadHDCT.Location = new System.Drawing.Point(1224, 274);
+            this.btn_LoadHDCT.Name = "btn_LoadHDCT";
+            this.btn_LoadHDCT.Size = new System.Drawing.Size(109, 48);
+            this.btn_LoadHDCT.TabIndex = 14;
+            this.btn_LoadHDCT.Text = "Load HĐCT";
+            this.btn_LoadHDCT.UseVisualStyleBackColor = true;
+            // 
+            // lb_MaHD
+            // 
+            this.lb_MaHD.AutoSize = true;
+            this.lb_MaHD.Location = new System.Drawing.Point(1094, 335);
+            this.lb_MaHD.Name = "lb_MaHD";
+            this.lb_MaHD.Size = new System.Drawing.Size(51, 20);
+            this.lb_MaHD.TabIndex = 10;
+            this.lb_MaHD.Text = "label6";
             // 
             // fStaffMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(1616, 619);
+            this.Controls.Add(this.lb_MaHD);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.flp_Phone);
+            this.Controls.Add(this.btn_ThanhToan);
+            this.Controls.Add(this.btn_NewHD);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.flp_HDCT);
+            this.Controls.Add(this.btn_LoadHDCT);
             this.Name = "fStaffMain";
             this.Text = "fStaffMain";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lb_Gia;
+        private System.Windows.Forms.Label lb_MaSP;
+        private System.Windows.Forms.Button btn_AddHDCT;
+        private System.Windows.Forms.Label lb_DM;
+        private System.Windows.Forms.Label lb_TenSP;
+        private System.Windows.Forms.Label lb_NCC;
+        private System.Windows.Forms.FlowLayoutPanel flp_Phone;
+        private System.Windows.Forms.Button btn_ThanhToan;
+        private System.Windows.Forms.Button btn_NewHD;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.FlowLayoutPanel flp_HDCT;
+        private System.Windows.Forms.Button btn_LoadHDCT;
+        private System.Windows.Forms.Label lb_MaHD;
     }
 }
